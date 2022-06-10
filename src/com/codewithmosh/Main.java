b1a540b7480be95f3483dfc43380f8170564fb7a
lmxy <wjzxyaowei@163.com>
2022-06-10 16:03:06 +0800

    balance of monthly payment

--- src/com/codewithmosh/Main.java
+++ src/com/codewithmosh/Main.java
@@ -4,6 +4,8 @@ import java.text.NumberFormat;
 import java.util.*;
 
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -36,16 +38,43 @@ public class Main {
         }
 
         double mortgage = calculateMortgage(principal, annualInterest, years);
+
         String mortgageFormatted = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
-        System.out.println("Mortgage: " + mortgageFormatted);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= years * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(balance));
+        }
+    }
+
+    public static double calculateBalance(
+            int principal,
+            float annualInterest,
+            byte years,
+            short numberOfPaymentsMade
+    ){
+
+        float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
     }
 
     public static double calculateMortgage(
             int principal,
             float annualInterest,
             byte years) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
 
         float monthlyInterest = annualInterest / MONTHS_IN_YEAR / PERCENT;
         short numberOfPayments = (short) (years * MONTHS_IN_YEAR);

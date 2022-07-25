4a101baeb9e543e9597a53c08cf7ac539eefde19
lmxy <wjzxyaowei@163.com>
2022-07-25 16:14:40 +0800

    save_0725

--- src/com/concurrency/DownloadStatus.java
+++ src/com/concurrency/DownloadStatus.java
@@ -1,13 +1,17 @@
 package com.concurrency;
 
+import java.util.concurrent.atomic.LongAdder;
+
 public class DownloadStatus {
-    private int totalBytes;
+    private boolean isDone;
+    private LongAdder totalBytes = new LongAdder();
+    private int totalFiles;
 
     public int getTotalBytes() {
-        return totalBytes;
+        return totalBytes.intValue();
     }
 
     public void incrementTotalBytes() {
-        totalBytes++;
+        totalBytes.increment();
     }
 }

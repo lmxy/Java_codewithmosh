7509c2a721ea1ada58a95a832662fef019bf2a47
lmxy <wjzxyaowei@163.com>
2022-07-26 22:46:56 +0800

    start-learn-thread-pool-final

--- src/com/executors/ExecutorsDemo.java
+++ src/com/executors/ExecutorsDemo.java
@@ -6,11 +6,16 @@ import java.util.concurrent.ThreadPoolExecutor;
 public class ExecutorsDemo {
     public static void show() {
         var executor = Executors.newFixedThreadPool(2);
-        for (int i = 0; i < 10; i++) {
+
+        try {
             executor.submit(() -> {
                 System.out.println(Thread.currentThread().getName());
             });
-
         }
+        finally {
+            executor.shutdown();
+        }
+
+
     }
 }
